c0b139eddc44c632bf06c0f5d819e9ce884f6915
kunal413 <kunalsharmaraj09@gmail.com>
2023-09-04 11:07:07 +0530

    change2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/studentDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB', { useNewUrlParser: true, useUnifiedTopology: true })
  .then(() => {
     console.log('Connected to MongoDB');
   })
